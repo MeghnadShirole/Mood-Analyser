5be86c7394222d8a248ed92290533874678da91b
MeghnadShirole <shirolemeghnad10@gmail.com>
2021-11-18 17:58:06 +0530

    Added:Custom Exception Handling

---
+++ src/main/java/com/company/MoodAnalysisException.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class MoodAnalysisException extends Exception {
+    public Exception_Type type;
+    public MoodAnalysisException(String message, Exception_Type type) {
+        super(message);
+    }
+
+    public enum Exception_Type {
+        NULL, EMPTY;
+    }
+}
--- src/test/java/com/company/MoodAnalyserTest.java
+++ src/test/java/com/company/MoodAnalyserTest.java
@@ -7,21 +7,35 @@ public class MoodAnalyserTest {
     @Test
     public void givenMoodSad_ShouldReturnSAD() {
         MoodAnalyser moodAnalyzer = new MoodAnalyser("I am In a Sad Mood");
-        String mood = moodAnalyzer.analyseMood();
-        Assert.assertEquals("SAD", mood);
+        String mood;
+        try {
+            mood = moodAnalyzer.analyseMood();
+            Assert.assertEquals("SAD", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void givenAnyMood_ShouldReturnHAPPY() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
-        String mood = moodAnalyser.analyseMood();
-        Assert.assertEquals("HAPPY", mood);
+        String mood;
+        try {
+            mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void given_NULLMood_Should_Return_HAPPY() {
+    public void givenNullMood_ShouldThrowMoodAnalysisException() {
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-        String mood = moodAnalyser.analyseMood();
-        Assert.assertEquals("HAPPY", mood);
+        String mood;
+        try {
+            mood = moodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals(MoodAnalysisException.Exception_Type.NULL,e.type);
+        }
     }
 }
