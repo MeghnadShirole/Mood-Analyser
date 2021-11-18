704d0ea7894cc5f44d32deb6a6453a4dc4a95806
MeghnadShirole <shirolemeghnad10@gmail.com>
2021-11-18 17:24:51 +0530

    Added:Ability to analyse and response Happy or Sad mood

--- src/main/java/com/company/MoodAnalyser.java
+++ src/main/java/com/company/MoodAnalyser.java
@@ -1,7 +1,15 @@
 package com.company;
 
 public class MoodAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Mood Analyser");
+
+    public MoodAnalyser() {
+
+    }
+
+    public String analyseMood(String message) {
+        if (message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
     }
 }
--- src/test/java/com/company/MoodAnalyserTest.java
+++ src/test/java/com/company/MoodAnalyserTest.java
@@ -1,4 +1,20 @@
 package com.company;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoodAnalyserTest {
+    @Test
+    public void givenMoodSad_ShouldReturnSAD() {
+        MoodAnalyser moodAnalyzer = new MoodAnalyser();
+        String mood = moodAnalyzer.analyseMood("Sad");
+        Assert.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void givenAnyMood_ShouldReturnHAPPY() {
+        MoodAnalyser moodAnalyzer = new MoodAnalyser();
+        String mood = moodAnalyzer.analyseMood("I am in Any Mood");
+        Assert.assertEquals("HAPPY", mood);
+    }
 }
